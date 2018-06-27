1d0585c279c3c44767315fb72a10543ce3a523b9
Soslan <kulumbekov1807@gmail.com>
2018-06-28 00:55:28 +0300

    fixed config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
-let path = require('path');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
 const config = {
     entry: './src/app.js',
     output: {
@@ -28,7 +33,26 @@ const config = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin("styles.css")
+        new ExtractTextPlugin("styles.css"),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+          }),
+        new CopyWebpackPlugin(
+          [
+            {
+              from: path.resolve(__dirname, 'assets'),
+              to: path.resolve(__dirname, 'dist/assets/')
+            },
+          ]),
+        new ImageminPlugin({
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          pngquant: {
+            quality: '95-100'
+          }
+        })
     ]
 }
 
